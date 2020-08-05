e7aba3322c5292751b062b4956b49b92f9ca5efa
kairzhan8 <kairzhankural@gmail.com>
2020-08-05 12:59:13 +0600

    Initial Commit

---
+++ Personality Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Personality Quiz
+//
+//  Created by Kairzhan Kural on 8/5/20.
+//  Copyright © 2020 Kairzhan Kural. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
