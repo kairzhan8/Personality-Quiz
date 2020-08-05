9b3d44049417c1c6389c314f797045acda39b2fa
kairzhan8 <kairzhankural@gmail.com>
2020-08-05 14:37:31 +0600

    setup model

---
+++ Personality Quiz/Model/Answer.swift
@@ -0,0 +1,12 @@
+//
+//  Answer.swift
+//  Personality Quiz
+//
+//  Created by Kairzhan Kural on 8/5/20.
+//  Copyright © 2020 Kairzhan Kural. All rights reserved.
+//
+
+struct Answer {
+    var text: String
+    var type: AnimalType
+}
---
+++ Personality Quiz/Model/ResponseType.swift
@@ -0,0 +1,13 @@
+//
+//  ResponseType.swift
+//  Personality Quiz
+//
+//  Created by Kairzhan Kural on 8/5/20.
+//  Copyright © 2020 Kairzhan Kural. All rights reserved.
+//
+
+enum ResponseType {
+    case single
+    case multiple
+    case ranged
+}
